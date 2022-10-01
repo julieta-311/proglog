11754444c3dcab0bb99335114e0c223e7e9882d5
Julieta Bnzz <wllwschrm@gmail.com>
2022-10-01 21:31:45 +0100

    api: update ErrOffsetOutOfRange to use grpc status

--- api/v1/error.go
+++ api/v1/error.go
@@ -1,14 +1,40 @@
 package log_v1
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	status "google.golang.org/grpc/status"
+)
 
 type ErrOffsetOutOfRange struct {
 	Offset uint64
 }
 
-func (e ErrOffsetOutOfRange) Error() string {
-	return fmt.Sprintf(
-		"offset out of range: %d",
+func (e ErrOffsetOutOfRange) GRPCStatus() *status.Status {
+	st := status.New(
+		http.StatusNotFound,
+		fmt.Sprintf("offset out of range: %d", e.Offset),
+	)
+
+	msg := fmt.Sprintf(
+		"The requested offset is outside the log's range: %d",
 		e.Offset,
 	)
+
+	d := &errdetails.LocalizedMessage{
+		Locale:  "en-GB",
+		Message: msg,
+	}
+	std, err := st.WithDetails(d)
+	if err != nil {
+		return st
+	}
+
+	return std
+}
+
+func (e ErrOffsetOutOfRange) Error() string {
+	return e.GRPCStatus().Err().Error()
 }
