a089db56a4f0bc939ffc1bc74825446c4e375667
Julieta Bnzz <wllwschrm@gmail.com>
2022-10-23 10:56:43 +0100

    server: add TLS authentication mechanism
    Update Makefile to generate TLS certificates and update the server
    config and test setup so that the server is authenticated and the
    connection is encrypted.

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -8,10 +8,11 @@ import (
 
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/status"
 
 	api "github.com/julieta-311/proglog/api/v1"
+	"github.com/julieta-311/proglog/internal/config"
 	"github.com/julieta-311/proglog/internal/log"
 )
 
@@ -22,16 +23,32 @@ func setupTest(t *testing.T, fn func(*Config)) (
 ) {
 	t.Helper()
 
-	l, err := net.Listen("tcp", ":0")
+	l, err := net.Listen("tcp", "127.0.0.1:0")
 	require.NoError(t, err)
 
-	clientOpts := []grpc.DialOption{
-		grpc.WithTransportCredentials(
-			insecure.NewCredentials(),
-		),
-	}
-	cc, err := grpc.Dial(l.Addr().String(), clientOpts...)
+	clientTLSConfig, err := config.SetupTLSConfig(
+		config.TLSConfig{
+			CAFile: config.CAFile,
+		})
 	require.NoError(t, err)
+
+	clientCreds := credentials.NewTLS(clientTLSConfig)
+	cc, err := grpc.Dial(
+		l.Addr().String(),
+		grpc.WithTransportCredentials(clientCreds))
+	require.NoError(t, err)
+
+	client = api.NewLogClient(cc)
+
+	serverTLSConfig, err := config.SetupTLSConfig(
+		config.TLSConfig{
+			CertFile:      config.ServerCertFile,
+			KeyFile:       config.ServerKeyFile,
+			CAFile:        config.CAFile,
+			ServerAddress: l.Addr().String(),
+		})
+	require.NoError(t, err)
+	serverCreds := credentials.NewTLS(serverTLSConfig)
 
 	dir, err := os.MkdirTemp("", "server-test")
 	require.NoError(t, err)
@@ -47,15 +64,15 @@ func setupTest(t *testing.T, fn func(*Config)) (
 		fn(cfg)
 	}
 
-	server, err := NewGRPCServer(cfg)
+	server, err := NewGRPCServer(
+		cfg,
+		grpc.Creds(serverCreds))
 	require.NoError(t, err)
 
 	go func() {
 		err := server.Serve(l)
 		require.NoError(t, err)
 	}()
-
-	client = api.NewLogClient(cc)
 
 	return client, cfg, func() {
 		server.Stop()
