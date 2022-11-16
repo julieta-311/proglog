831758d7e77515dc21b7751bdd7b2c401878d49e
Julieta Bnzz <wllwschrm@gmail.com>
2022-11-16 20:43:06 +0000

    server: implement authorization
    Implement a system to determin if the client has permissions to execute
    the requested action.

--- internal/server/server.go
+++ internal/server/server.go
@@ -3,12 +3,30 @@ package server
 import (
 	"context"
 
-	api "github.com/julieta-311/proglog/api/v1"
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
+	grpc_auth "github.com/grpc-ecosystem/go-grpc-middleware/auth"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/peer"
+	"google.golang.org/grpc/status"
+
+	api "github.com/julieta-311/proglog/api/v1"
 )
 
+const (
+	objectWildcard = "*"
+	produceAction  = "produce"
+	consumeAction  = "consume"
+)
+
+type authorizer interface {
+	Authorize(subject, object, action string) error
+}
+
 type Config struct {
-	CommitLog CommitLog
+	CommitLog  CommitLog
+	Authorizer authorizer
 }
 
 var _ api.LogServer = (*grpcServer)(nil)
@@ -31,6 +49,15 @@ func NewGRPCServer(
 	config *Config,
 	opts ...grpc.ServerOption,
 ) (*grpc.Server, error) {
+	opts = append(opts, grpc.StreamInterceptor(
+		grpc_middleware.ChainStreamServer(
+			grpc_auth.StreamServerInterceptor(authenticate),
+		)),
+		grpc.UnaryInterceptor(
+			grpc_middleware.ChainUnaryServer(
+				grpc_auth.UnaryServerInterceptor(authenticate),
+			)))
+
 	gsrv := grpc.NewServer(opts...)
 
 	srv, err := newgrpcServer(config)
@@ -52,6 +79,14 @@ func (s *grpcServer) Produce(ctx context.Context, req *api.ProduceRequest) (
 	*api.ProduceResponse,
 	error,
 ) {
+	if err := s.Authorizer.Authorize(
+		subject(ctx),
+		objectWildcard,
+		produceAction,
+	); err != nil {
+		return nil, err
+	}
+
 	offset, err := s.CommitLog.Append(req.Record)
 	if err != nil {
 		return nil, err
@@ -67,6 +102,14 @@ func (s *grpcServer) Consume(ctx context.Context, req *api.ConsumeRequest) (
 	*api.ConsumeResponse,
 	error,
 ) {
+	if err := s.Authorizer.Authorize(
+		subject(ctx),
+		objectWildcard,
+		consumeAction,
+	); err != nil {
+		return nil, err
+	}
+
 	record, err := s.CommitLog.Read(req.Offset)
 	if err != nil {
 		return nil, err
@@ -132,3 +175,42 @@ func (s *grpcServer) ConsumeStream(
 		}
 	}
 }
+
+// authenticate is an interceptor that reads the subject out of
+// the client's cert and writes it to the RPC's context.
+func authenticate(ctx context.Context) (context.Context, error) {
+	peer, ok := peer.FromContext(ctx)
+	if !ok {
+		return ctx, status.New(
+			codes.Unknown,
+			"could not find peer info",
+		).Err()
+	}
+
+	if peer.AuthInfo == nil {
+		return context.WithValue(
+			ctx,
+			subjectContextKey{},
+			"",
+		), nil
+	}
+
+	tlsInfo := peer.AuthInfo.(credentials.TLSInfo)
+	subject := tlsInfo.State.VerifiedChains[0][0].Subject.CommonName
+	ctx = context.WithValue(
+		ctx,
+		subjectContextKey{},
+		subject,
+	)
+
+	return ctx, nil
+}
+
+// subject returns the client's cert's subject
+// so it can be used to identify a client and
+// verify their access.
+func subject(ctx context.Context) string {
+	return ctx.Value(subjectContextKey{}).(string)
+}
+
+type subjectContextKey struct{}
