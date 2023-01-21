c8faed0a07a0e6562c28907fd41a5885a26957ea
Julieta Bnzz <wllwschrm@gmail.com>
2023-01-21 18:54:27 +0000

    internal/log: implement GetServers method

--- internal/log/distributed.go
+++ internal/log/distributed.go
@@ -266,3 +266,22 @@ func (l *DistributedLog) Close() error {
 	}
 	return l.log.Close()
 }
+
+// GetServers exposes Raft's server data.
+func (l *DistributedLog) GetServers() ([]*api.Server, error) {
+	future := l.raft.GetConfiguration()
+	if err := future.Error(); err != nil {
+		return nil, err
+	}
+
+	var servers []*api.Server
+	for _, server := range future.Configuration().Servers {
+		servers = append(servers, &api.Server{
+			Id:       string(server.ID),
+			RpcAddr:  string(server.Address),
+			IsLeader: l.raft.Leader() == server.Address,
+		})
+	}
+
+	return servers, nil
+}
