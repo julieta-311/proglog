9a3c20e218418c49ef4e29ca5c8e23690c940e31
Julieta Bnzz <wllwschrm@gmail.com>
2023-03-29 12:33:45 +0100

    cli: implement an agent command-line interface
    The agent CLI being added provides just enough features to use as a
    Docker image's entry point and run the service, parse flags and
    configure and run the agent.

---
+++ cmd/proglog/main.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"path"
+	"syscall"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+
+	"github.com/julieta-311/proglog/internal/agent"
+	"github.com/julieta-311/proglog/internal/config"
+)
+
+func main() {
+	cli := &cli{}
+
+	cmd := &cobra.Command{
+		Use:     "proglog",
+		PreRunE: cli.setupConfig,
+		RunE:    cli.run,
+	}
+
+	if err := setupFlags(cmd); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := cmd.Execute(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func setupFlags(cmd *cobra.Command) error {
+	hostname, err := os.Hostname()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cmd.Flags().String("config-file", "", "Path to config file.")
+
+	cmd.Flags().String(
+		"data-dir",
+		path.Join(os.TempDir(), "proglog"),
+		"Path to the directory where to store log and Raft data.",
+	)
+
+	cmd.Flags().String("node-name", hostname, "Unique server ID.")
+
+	cmd.Flags().String("bind-addr", "127.0.0.1:8401", "Address to bind Serf on.")
+	cmd.Flags().Int("rpc-port", 8400, "Port for RPC clients (and Raft) connections.")
+	cmd.Flags().StringSlice("start-join-addrs", nil, "Serf addresses to join.")
+	cmd.Flags().Bool("bootstrap", false, "Bootstrap the cluster.")
+
+	cmd.Flags().String("acl-model-file", "", "Path to ACL model.")
+	cmd.Flags().String("acl-policy-file", "", "Path to ACL policy.")
+
+	cmd.Flags().String("peer-tls-cert-file", "", "Path to peer tls cert.")
+	cmd.Flags().String("peer-tls-key-file", "", "Path to peer tls key")
+	cmd.Flags().String("peer-tls-ca-file", "", "Path to peer certificate authority.")
+
+	return viper.BindPFlags(cmd.Flags())
+}
+
+type cli struct {
+	cfg cfg
+}
+
+type cfg struct {
+	agent.Config
+	ServerTLSConfig config.TLSConfig
+	PeerTLSConfig   config.TLSConfig
+}
+
+// setupConfig reads the configuration and prepares the agent's configuration.
+// Cobra calls setupConfig before running the command's RunE function.
+func (c *cli) setupConfig(cmd *cobra.Command, args []string) error {
+	configFile, err := cmd.Flags().GetString("config-file")
+	if err != nil {
+		return err
+	}
+	viper.SetConfigFile(configFile)
+
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			return err
+		}
+	}
+
+	c.cfg.DataDir = viper.GetString("data-dir")
+	c.cfg.NodeName = viper.GetString("node-name")
+	c.cfg.BindAddr = viper.GetString("bind-addr")
+	c.cfg.RPCPort = viper.GetInt("rpc-port")
+	c.cfg.StartJoinAddrs = viper.GetStringSlice("start-join-addrs")
+	c.cfg.Bootstrap = viper.GetBool("bootstrap")
+	c.cfg.ACLModelFile = viper.GetString("acl-model-file")
+	c.cfg.ACLPolicyFile = viper.GetString("acl-policy-file")
+	c.cfg.ServerTLSConfig.CertFile = viper.GetString("server-tls-cert-file")
+	c.cfg.ServerTLSConfig.KeyFile = viper.GetString("server-tls-key-file")
+	c.cfg.ServerTLSConfig.CAFile = viper.GetString("server-tls-ca-file")
+	c.cfg.PeerTLSConfig.CertFile = viper.GetString("peer-tls-cert-file")
+	c.cfg.PeerTLSConfig.KeyFile = viper.GetString("peer-tls-key-file")
+	c.cfg.PeerTLSConfig.CAFile = viper.GetString("peer-tls-ca-file")
+
+	if c.cfg.ServerTLSConfig.CertFile != "" && c.cfg.ServerTLSConfig.KeyFile != "" {
+		c.cfg.ServerTLSConfig.Server = true
+
+		var err error
+		c.cfg.Config.ServerTLSConfig, err = config.SetupTLSConfig(
+			c.cfg.ServerTLSConfig,
+		)
+		if err != nil {
+			return err
+		}
+	}
+
+	if c.cfg.PeerTLSConfig.CertFile != "" && c.cfg.PeerTLSConfig.KeyFile != "" {
+		var err error
+		c.cfg.Config.PeerTLSConfig, err = config.SetupTLSConfig(
+			c.cfg.PeerTLSConfig,
+		)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// run creates the agent, handles signals from the operating system,
+// and handles shut down signals gracefully.
+func (c *cli) run(cmd *cobra.Command, args []string) error {
+	agent, err := agent.New(c.cfg.Config)
+	if err != nil {
+		return err
+	}
+
+	sigc := make(chan os.Signal, 1)
+	signal.Notify(sigc, syscall.SIGINT, syscall.SIGTERM)
+	<-sigc
+
+	return agent.Shutdown()
+}
--- go.mod
+++ go.mod
@@ -5,45 +5,64 @@ go 1.20
 require (
 	github.com/casbin/casbin v1.9.1
 	github.com/gorilla/mux v1.8.0
-	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0
-	github.com/hashicorp/raft v1.3.11
+	github.com/grpc-ecosystem/go-grpc-middleware v1.4.0
+	github.com/hashicorp/raft v1.4.0
 	github.com/hashicorp/raft-boltdb/v2 v2.2.2
 	github.com/hashicorp/serf v0.10.1
 	github.com/soheilhy/cmux v0.1.5
-	github.com/stretchr/testify v1.8.1
+	github.com/spf13/cobra v1.6.1
+	github.com/spf13/viper v1.15.0
+	github.com/stretchr/testify v1.8.2
 	github.com/travisjeffery/go-dynaport v1.0.0
 	github.com/tysonmote/gommap v0.0.2
 	go.opencensus.io v0.24.0
 	go.uber.org/zap v1.24.0
-	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013
-	google.golang.org/grpc v1.49.0
-	google.golang.org/protobuf v1.28.1
+	google.golang.org/genproto v0.0.0-20230327215041-6ac7f18bb9d5
+	google.golang.org/grpc v1.54.0
+	google.golang.org/protobuf v1.30.0
 )
 
 require (
 	github.com/Knetic/govaluate v3.0.1-0.20171022003610-9aa49832a739+incompatible // indirect
-	github.com/armon/go-metrics v0.3.8 // indirect
+	github.com/armon/go-metrics v0.4.1 // indirect
 	github.com/boltdb/bolt v1.3.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/google/btree v0.0.0-20180813153112-4030bb1f1f0c // indirect
-	github.com/hashicorp/errwrap v1.0.0 // indirect
-	github.com/hashicorp/go-hclog v0.9.1 // indirect
-	github.com/hashicorp/go-immutable-radix v1.0.0 // indirect
+	github.com/fatih/color v1.15.0 // indirect
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/btree v1.1.2 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-hclog v1.5.0 // indirect
+	github.com/hashicorp/go-immutable-radix v1.3.1 // indirect
 	github.com/hashicorp/go-msgpack v0.5.5 // indirect
-	github.com/hashicorp/go-multierror v1.1.0 // indirect
-	github.com/hashicorp/go-sockaddr v1.0.0 // indirect
-	github.com/hashicorp/golang-lru v0.5.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/hashicorp/go-sockaddr v1.0.2 // indirect
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/hashicorp/memberlist v0.5.0 // indirect
-	github.com/miekg/dns v1.1.41 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.18 // indirect
+	github.com/miekg/dns v1.1.53 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.7 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sean-/seed v0.0.0-20170313163322-e2103e2c3529 // indirect
-	go.etcd.io/bbolt v1.3.6 // indirect
-	go.uber.org/atomic v1.7.0 // indirect
-	go.uber.org/multierr v1.6.0 // indirect
-	golang.org/x/net v0.7.0 // indirect
-	golang.org/x/sys v0.5.0 // indirect
-	golang.org/x/text v0.7.0 // indirect
+	github.com/spf13/afero v1.9.5 // indirect
+	github.com/spf13/cast v1.5.0 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/subosito/gotenv v1.4.2 // indirect
+	go.etcd.io/bbolt v1.3.7 // indirect
+	go.uber.org/atomic v1.10.0 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	golang.org/x/mod v0.9.0 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
+	golang.org/x/tools v0.7.0 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
