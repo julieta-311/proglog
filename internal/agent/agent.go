63a776318b604876e90eac9e77530a824856a098
Julieta Bnzz <wllwschrm@gmail.com>
2023-04-02 07:44:57 +0100

    internal/log: advertise raft on fully qualified domain name
    Currently, Raft's address is configured as the transport's local
    address, and the server will advertise its address ass ::8400. In order
    to use a fully qualified domain name so the node will properly advertise
    itself to its cluster and to its client, add a bind address field to the
    raft configuration struct.

--- internal/agent/agent.go
+++ internal/agent/agent.go
@@ -107,10 +107,12 @@ func (a *Agent) setupLogger() error {
 }
 
 func (a *Agent) setupMux() error {
-	rpcAddr := fmt.Sprintf(
-		":%d",
-		a.Config.RPCPort,
-	)
+	addr, err := net.ResolveTCPAddr("tcp", a.Config.BindAddr)
+	if err != nil {
+		return err
+	}
+
+	rpcAddr := fmt.Sprintf("%s:%d", addr.IP.String(), a.Config.RPCPort)
 	ln, err := net.Listen("tcp", rpcAddr)
 	if err != nil {
 		return err
@@ -136,12 +138,18 @@ func (a *Agent) setupLog() error {
 		a.Config.ServerTLSConfig,
 		a.Config.PeerTLSConfig,
 	)
+
+	rpcAddr, err := a.Config.RPCAddr()
+	if err != nil {
+		return err
+	}
+	logConfig.Raft.BindAddr = rpcAddr
+
 	logConfig.Raft.LocalID = raft.ServerID(
 		a.Config.NodeName,
 	)
 	logConfig.Raft.Bootstrap = a.Config.Bootstrap
 
-	var err error
 	a.log, err = log.NewDistributedLog(
 		a.Config.DataDir,
 		logConfig,
